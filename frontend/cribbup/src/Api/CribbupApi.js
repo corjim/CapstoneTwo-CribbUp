7ae7335574dc58fdb74a65b96f63814fd4fa8dc6
MartinOrji <martin.orji.jnr@gmail.com>
2025-04-02 22:19:08 -0500

    module install

--- frontend/cribbup/src/Api/CribbupApi.js
+++ frontend/cribbup/src/Api/CribbupApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_BASE_URL || "https://cribbup.onrender.com/"
+const API_BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 class CribbUpApi {
 
